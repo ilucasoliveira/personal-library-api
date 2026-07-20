99caea6a4aac3e9b72ec1fc1b0a9054dc5e37c51
Lucas <lucasdeoliveira937@gmail.com>
2026-07-20 10:07:58 -0300

    feat: username and password changed.

--- src/back_end_library/auth.py
+++ src/back_end_library/auth.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 security = HTTPBasic()
 
-USUARIO = os.getenv("MEU_USUARIO")
-SENHA = os.getenv("MINHA_SENHA")
+USERNAME = os.getenv("APP_USERNAME")
+PASSWORD = os.getenv("APP_PASSWORD")
 
 def user_authenticate(credentials: HTTPBasicCredentials=Depends(security)):
-    is_username_correct = compare_digest(credentials.username, USUARIO)
-    is_password_correct = compare_digest(credentials.password, SENHA)
+    is_username_correct = compare_digest(credentials.username, USERNAME)
+    is_password_correct = compare_digest(credentials.password, PASSWORD)
     
     if not (is_username_correct and is_password_correct):
         raise HTTPException(status_code=401, detail="Unauthorized credentials", headers={"WWW-Authenticate":"Basic"})
